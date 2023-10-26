718496a8313e176d0dc414f2df57ace109f27d63
Dragan <milosevicdragan002@gmail.com>
2023-10-26 16:43:58 +0200

    Create model

--- main.py
+++ main.py
@@ -13,3 +13,21 @@ print("X_train.shape:",X_train.shape)
 X_train = np.expand_dims(X_train,-1)
 X_test = np.expand_dims(X_test,-1)
 print(X_train.shape)
+
+K = len(set(y_train))
+print("Number of classes:",K)
+
+from keras.api._v2.keras import activations
+i = Input(shape = X_train[0].shape)
+x = Conv2D(32,(3,3),strides = 2,activation = 'relu')(i)
+x = Conv2D(64,(3,3),strides = 2,activation = 'relu')(x)
+x = Conv2D(128,(3,3),strides = 2,activation = 'relu')(x)
+x = Flatten()(x)
+x = Dropout(0.2)(x)
+x = Dense(512,activation = 'relu')(x)
+x = Dropout(0.2)(x)
+x = Dense(K,activation = 'softmax')(x)
+
+model = Model(i,x)
+
+

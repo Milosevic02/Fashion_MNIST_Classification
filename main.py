83f5e9e493031e0887288e0dad51dddc58bb622a
Dragan <milosevicdragan002@gmail.com>
2023-10-26 16:41:56 +0200

    expand dims

--- main.py
+++ main.py
@@ -9,3 +9,7 @@ fashion_mnist = tf.keras.datasets.fashion_mnist
 (X_train,y_train),(X_test,y_test) = fashion_mnist.load_data()
 X_train,X_test = X_train/255.0,X_test/255.0
 print("X_train.shape:",X_train.shape)
+
+X_train = np.expand_dims(X_train,-1)
+X_test = np.expand_dims(X_test,-1)
+print(X_train.shape)
